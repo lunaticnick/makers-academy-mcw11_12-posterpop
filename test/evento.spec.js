5ab1f03d79f98dc82796916fe28a795fbe8cf9ac
Nikolaos <lunaticnick@hotmail.com>
2018-02-06 15:39:25 +0000

    Set-up testing frameworks

---
+++ test/evento.spec.js
@@ -0,0 +1,12 @@
+import {mount} from 'vue-test-utils';
+import Evento from '../src/components/evento.js';
+import expect from 'expect';
+
+describe ("Evento", () => {
+ it ("EXPECTATION", () => {
+   let wrapper = mount(Evento);
+
+   expect(TESTED_ELEMENT).to ASSERTION
+ });
+
+});
