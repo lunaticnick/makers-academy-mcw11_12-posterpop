b65dc737f5abca49975cb47866bdf762b24421e7
Léa R <lea.rosamond@gmail.com>
2018-02-07 14:11:03 +0000

    Setting up the vue, adding a Hi message and a capture button

---
+++ src/components/CapturePhoto.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="capture-photo">
+    <h1>{{ msg }}</h1>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CapturePhoto',
+  data() {
+
+  },
+};
+</script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="welcome-message">
+      <h1>{{ msg }}</h1>
+    </div>
+    <div class="capture-photo">
+      <button>capture</button>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      msg: 'Hi!',
+    };
+  },
+};
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
