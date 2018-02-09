346e5fc48dc10bf119838606f85efe28d184fa7c
Nikolaos <lunaticnick@hotmail.com>
2018-02-09 14:15:46 +0000

    added the ability to take a picture
    * In case of PC it just uploads the picture
    * In case of mobile devices it prompts for what device to use

--- src/components/CapturePhoto.vue
+++ src/components/CapturePhoto.vue
@@ -1,35 +1,73 @@
 <template>
 <div class="capture-photo">
-  <video id="video" controls autoplay></video>
+  <video id="video" controls autoplay width=150 height=150></video>
+  <canvas id="canvas" width=180 height=150></canvas>
   <button id="capture" v-on:click="capture">Capture</button>
-  <canvas id="canvas" width=340 height=240></canvas>
+<br/>
+<hr>
+
+<input
+      type="file"
+      accept="image/*"
+      capture="environment"
+      @change="onImageCaptured($event.target.name, $event.target.files)">
+    <img v-bind:src="imgURL" v-if="imgURL" style="width:200px">
+
 </div>
 </template>
 
 <script>
 export default {
   name: 'CapturePhoto',
+  data() {
+    return {
+      imgURL: null,
+    };
+  },
   methods: {
     capture() {
+      const video = document.getElementById('video');
+      const canvas = document.getElementById('canvas'); // eslint-disable-line no-unused-vars
       const context = canvas.getContext('2d');
       context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    },
+
+    getBase64Image(file) {
+      return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+        fileReader.addEventListener('load', () => {
+          resolve(fileReader.result);
+        });
+        fileReader.addEventListener('error', (err) => {
+          reject(err);
+        });
+        fileReader.readAsDataURL(file);
+      });
+    },
+
+    onImageCaptured(name, files) {
+      this.getBase64Image(files[0])
+        .then((url) => {
+          this.imgURL = url;
+        });
     },
   },
 };
 
 window.addEventListener('load', () => {
   const video = document.getElementById('video');
-  const canvas = document.getElementById('canvas'); // eslint-disable-line no-unused-vars
-  const captureButton = document.getElementById('capture'); // eslint-disable-line no-unused-vars
+  const fileInput = document.getElementById('file-input');
 
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     navigator.mediaDevices.getUserMedia({
       video: true,
-      facingMode: { exact: "environment"},
     }).then((stream) => {
       video.src = window.URL.createObjectURL(stream);
       video.play();
     });
   }
+
+
+
 });
 </script>
