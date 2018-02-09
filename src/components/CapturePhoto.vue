1a81177bfd8a73fa5f2685bad2fcb60188b0d8e0
Nikolaos <lunaticnick@hotmail.com>
2018-02-09 14:57:30 +0000

    Photo can be acquired - closes #5

--- src/components/CapturePhoto.vue
+++ src/components/CapturePhoto.vue
@@ -1,16 +1,12 @@
 <template>
 <div class="capture-photo">
-  <video id="video" controls autoplay width=150 height=150></video>
-  <canvas id="canvas" width=180 height=150></canvas>
-  <button id="capture" v-on:click="capture">Capture</button>
-<br/>
-<hr>
 
 <input
       type="file"
       accept="image/*"
       capture="environment"
       @change="onImageCaptured($event.target.name, $event.target.files)">
+
     <img v-bind:src="imgURL" v-if="imgURL" style="width:200px">
 
 </div>
@@ -25,12 +21,6 @@ export default {
     };
   },
   methods: {
-    capture() {
-      const video = document.getElementById('video');
-      const canvas = document.getElementById('canvas'); // eslint-disable-line no-unused-vars
-      const context = canvas.getContext('2d');
-      context.drawImage(video, 0, 0, canvas.width, canvas.height);
-    },
 
     getBase64Image(file) {
       return new Promise((resolve, reject) => {
@@ -49,25 +39,11 @@ export default {
       this.getBase64Image(files[0])
         .then((url) => {
           this.imgURL = url;
+          console.log(this.imgURL);
         });
     },
   },
 };
 
-window.addEventListener('load', () => {
-  const video = document.getElementById('video');
-  const fileInput = document.getElementById('file-input');
 
-  if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({
-      video: true,
-    }).then((stream) => {
-      video.src = window.URL.createObjectURL(stream);
-      video.play();
-    });
-  }
-
-
-
-});
 </script>
