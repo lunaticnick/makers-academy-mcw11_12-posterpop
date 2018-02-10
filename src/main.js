7a019ef528fa1ea636355bfaba6fba96084bc456
Keran <kerbraich@gmail.com>
2018-02-10 20:12:51 +0000

    Add Firebase config to main.js

--- src/main.js
+++ src/main.js
@@ -3,14 +3,24 @@ import VueResource from 'vue-resource';
 import BootstrapVue from 'bootstrap-vue';
 import App from './App';
 import router from './router';
+import firebase from 'firebase';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-// import './style.css';
 
 Vue.use(VueResource);
 Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+
+  var config = {
+    apiKey: "AIzaSyDM9yYNwqxpiAxck0c47649KrIxOlR-m0M",
+    authDomain: "root-bricolage-194510.firebaseapp.com",
+    databaseURL: "https://root-bricolage-194510.firebaseio.com",
+    projectId: "root-bricolage-194510",
+    storageBucket: "root-bricolage-194510.appspot.com",
+    messagingSenderId: "308649884099"
+  };
+  firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
