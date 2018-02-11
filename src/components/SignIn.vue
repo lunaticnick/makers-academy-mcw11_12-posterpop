97a4ce4f4a1e23acde174a5ca82f278315dbb08a
Lan Pham <lanpham312@gmail.com>
2018-02-11 16:50:04 +0000

    Passing test for showing the sign up page

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -22,5 +22,23 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,36 @@
+<template lang="html">
+  <div class="">
+    <form class="signUpForm">
+      <p>Please create a new account below</p>
+
+    </form>
+    <button>Sign Up</button>
+  </div>
+
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
