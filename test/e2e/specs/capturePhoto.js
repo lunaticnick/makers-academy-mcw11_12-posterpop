cd22f17237df3f2bdc77e1cc380b93e0388d804d
Léa R <lea.rosamond@gmail.com>
2018-02-12 10:57:42 +0000

    Passing the feature test for displaying the extracted url link from the poster. Importing an 'dummy' poster from internet. thank you very much

--- test/e2e/specs/capturePhoto.js
+++ test/e2e/specs/capturePhoto.js
@@ -1,13 +1,14 @@
 module.exports = {
-  'Shows The extrapolated url link': function test(browser) {
+  'Shows The extracted url ".uk" link': function test(browser) {
     const devServer = browser.globals.devServerURL;
 
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
       .click('input[id="chooseFile"]')
-      .setValue('input[type="file"]', require('path').resolve('https://i.imgur.com/xo03IMi.jpg'))
+      .setValue('input[type="file"]', require('path').resolve('http://payload536.cargocollective.com/1/3/108157/13033615/Screen-Shot-2017-12-13-at-14.48.52_920.png'))
       .click("#sendImage")
-      .assert.elementPresent('#extrapolatedLink', 'https')
+      .assert.elementPresent('#extractedLink', 'http://nhm.ac.uk')
       .end();
   },
+};
