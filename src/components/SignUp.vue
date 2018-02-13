566b925ab7198d3547f9e113f8a282178f48d9ad
Lan Pham <lanpham312@gmail.com>
2018-02-13 11:57:41 +0000

    Adding the redirection from the sign up page to the home page

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -25,10 +25,10 @@ export default {
   methods: {
     signUp: function () {
       firebase.auth().createUserWithEmailAndPassword(this.email, this.password).then(
-        function (user) {
-          alert('Your account has successfully been created.')
+        (user) => {
+          this.$router.replace('home')
         },
-        function (err) {
+        (err) => {
           alert(err.message)
         }
       );
